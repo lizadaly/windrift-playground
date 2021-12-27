3c801c99137c3ea84889dfbb639c5e6375eec0dd
Liza Daly <lizadaly@gmail.com>
2021-12-27 09:13:55 -0500

    Demonstrate using a twitter card for a story

--- stories/time-machine/index.tsx
+++ stories/time-machine/index.tsx
@@ -14,10 +14,19 @@ const Index: React.FC = ({ children }) => {
         <Grid
             styles={styles}
             head={
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto+Slab&family=Roboto+Condensed&family=Smythe&family=Orbitron&family=Noto+Sans+Cuneiform&family=Macondo+Swash+Caps&display=swap"
-                    rel="stylesheet"
-                />
+                <>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Roboto+Slab&family=Roboto+Condensed&family=Smythe&family=Orbitron&family=Noto+Sans+Cuneiform&family=Macondo+Swash+Caps&display=swap"
+                        rel="stylesheet"
+                    />
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:creator" content="@liza" />
+                    <meta name="twitter:title" content="Trapped in Your Time Machine!" />
+                    <meta
+                        name="twitter:description"
+                        content="A demo multimedia interactive fiction hypertext story for the Windrift framework"
+                    />
+                </>
             }
             header={<></>}
             right={
